7ed5fe16feef416fe89f73709d20d6023de82e81
lender512 <luis.berrospi@utec.edu.pe>
2024-08-21 00:58:50 -0500

    ✨ Line charts

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ import numpy as np
 
 df = pd.read_csv('data/raw/players.csv')
 df['win_rate'] = df['matches_won'] / df['matches_played']
+seasonal_df = pd.read_csv('data/raw/players_season.csv')
+seasonal_df['win_rate'] = seasonal_df['matches_won'] / seasonal_df['matches_played']
+
 # name,id,matches_played,matches_won,matches_lost,player_kills_kda,player_kills_kills,player_kills_deaths,player_kills_assists,farming_gpm,farming_gold,farming_minion_damage,farming_minion_kills,damage_player,damage_team_healing,damage_self_healing,damage_inhand,damage_mitigated,damage_taken
 
 rename_dic = {  'name': 'Player name',
@@ -30,6 +33,7 @@ rename_dic = {  'name': 'Player name',
 
 st.title('SMITE: Smoteros players stats')
 
+
 match_columns = ['matches_played', 'matches_won', 'matches_lost', 'win_rate']
 kda_columns = ['player_kills_kda', 'player_kills_kills', 'player_kills_deaths', 'player_kills_assists']
 farming_columns = ['farming_gpm', 'farming_gold', 'farming_minion_damage', 'farming_minion_kills']
@@ -44,4 +48,12 @@ grids = {
 
 for grid in grids:
     st.subheader(grid)
-    st.write(df[['name'] + grids[grid]].rename(columns=rename_dic), hide_index=True)
+    st.write(df[['name'] + grids[grid]].rename(columns=rename_dic).set_index(rename_dic['name']), hide_index=True)
+
+
+    stat = st.selectbox('Select a stat', grids[grid], format_func=lambda x: rename_dic[x])
+
+    temp_df = seasonal_df[['name', 'season', stat]].groupby(['season', 'name']).sum().unstack().fillna(0)
+    #flatten the columns
+    temp_df.columns = temp_df.columns.get_level_values(1)
+    st.line_chart(temp_df)
